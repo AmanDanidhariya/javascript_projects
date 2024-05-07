135a0f4f6e700600cef62178194c7a820422d65d
Aman Danidhariya <amandanidhariya007@gmail.com>
2024-05-07 13:30:33 +0530

    styling a ExpandingCards

---
+++ 3.Expanding_cards/script.js
@@ -0,0 +1,29 @@
+const images = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1714685953621-4e7ae070ee02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D",
+    text: "first_image",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1714646542331-278803db302f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
+    text: "second_image",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1714332818313-627551693dbc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHx8fA%3D%3D",
+    text: "third_image",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1714952226774-b7a2c5707e30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8fA%3D%3D",
+    text: "fourth_image",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1714383524948-ebc87c14c0f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Mnx8fGVufDB8fHx8fA%3D%3D",
+    text: "fifth_image",
+  },
+];
+
+console.log(images);
