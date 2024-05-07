443fd18d64fe7e6e448c481a9e4bb3fb5c33aa44
Aman Danidhariya <amandanidhariya007@gmail.com>
2024-05-07 14:07:53 +0530

    adding active class functionality to expand cards

--- 3.Expanding_cards/script.js
+++ 3.Expanding_cards/script.js
@@ -26,4 +26,14 @@ const images = [
   },
 ];
 
-console.log(images);
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+
+    panel.classList.add("active");
+  });
+});
