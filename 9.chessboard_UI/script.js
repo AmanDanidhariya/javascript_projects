85a434f9756f92b8b3007cecb256219191912ced
Aman Danidhariya <amandanidhariya007@gmail.com>
2024-05-20 00:03:29 +0530

    make chessboard UI flexible with userInput

--- 9.chessboard_UI/script.js
+++ 9.chessboard_UI/script.js
@@ -1,17 +1,39 @@
 let table = document.querySelector(".table");
+let textBoxValue;
+let getValue = document.querySelector(".getValue");
 
-for (let i = 0; i < 8; i++) {
-  let tr = document.createElement("tr");
-  let white = i % 2 === 0 ? true : false;
-  for (let j = 0; j < 8; j++) {
-    let td = document.createElement("td");
-    if (white) {
-      td.setAttribute("class", "box white");
-    } else {
-      td.setAttribute("class", "box black");
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("getValue").addEventListener("click", function () {
+    textBoxValue = document.querySelector(".textbox").value;
+    for (let i = 0; i < textBoxValue; i++) {
+      let tr = document.createElement("tr");
+      let white = i % 2 === 0 ? true : false;
+      for (let j = 0; j < textBoxValue; j++) {
+        let td = document.createElement("td");
+        if (white) {
+          td.setAttribute("class", "box white");
+        } else {
+          td.setAttribute("class", "box black");
+        }
+        white = !white;
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
     }
-    white=!white
-    tr.appendChild(td);
-  }
-  table.appendChild(tr);
-}
+  });
+});
+// for (let i = 0; i < 8; i++) {
+//   let tr = document.createElement("tr");
+//   let white = i % 2 === 0 ? true : false;
+//   for (let j = 0; j < 8; j++) {
+//     let td = document.createElement("td");
+//     if (white) {
+//       td.setAttribute("class", "box white");
+//     } else {
+//       td.setAttribute("class", "box black");
+//     }
+//     white = !white;
+//     tr.appendChild(td);
+//   }
+//   table.appendChild(tr);
+// }
